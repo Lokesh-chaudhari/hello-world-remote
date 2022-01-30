30f082aab5e2a5f26ada4bce57df317b2f04e7c2
Lokesh-chaudhari <98691558+Lokesh-chaudhari@users.noreply.github.com>
2022-01-30 17:59:10 +0530

    updated math.py to basic

--- math.py
+++ math.py
@@ -1,14 +1,8 @@
-#funtion for addition
 def add(x,y):
     pass
-#funtion for sub
 def sub(x,y):
-    return x-y  #on master branch
+    pass
 def mul(x,y):
-    return x*y  #in bug456
+    pass
 def div(x,y):
-    if y==0:
-        return "error dividing by 0"
-    else:
-        return x/y
-
+   pass
