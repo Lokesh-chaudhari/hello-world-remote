0481eed7efeea58c99d19edba25e6df92ea43e5d
Lokesh-chaudhari <lokesh.chaudhari9191@gmail.com>
2022-01-30 17:44:47 +0530

    inserted funtion header for all

--- math.py
+++ math.py
@@ -1,8 +1,12 @@
+#add implementation
 def add(x,y):
     pass
+#sub implementation
 def sub(x,y):
     pass
+#mul implementation
 def mul(x,y):
     pass
+#div implementation
 def div(x,y):
    pass
