678ee881aeb8cc03bd9bdb62a1663a048512f183
Lokesh-chaudhari <98691558+Lokesh-chaudhari@users.noreply.github.com>
2022-01-30 17:57:26 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,14 @@
+#funtion for addition
+def add(x,y):
+    pass
+#funtion for sub
+def sub(x,y):
+    return x-y  #on master branch
+def mul(x,y):
+    return x*y  #in bug456
+def div(x,y):
+    if y==0:
+        return "error dividing by 0"
+    else:
+        return x/y
+
